4d55aa985fd2475494692e781e7d11df342e0b1d
rintaromizukami <rintaronakai@gmail.com>
2024-06-22 23:41:52 +0900

    change port number from 80 to 8080

--- app.js
+++ app.js
@@ -293,7 +293,7 @@ app.post('/generate-pdf', upload.array("photos", 5), async (req, res) => {
         }
     });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
