692cf7c85d8b54fc45258c85c5f957d500b1f4c9
rintaromizukami <rintaronakai@gmail.com>
2024-06-23 16:25:31 +0900

    change config of html

--- app.js
+++ app.js
@@ -268,8 +268,12 @@ app.post('/generate-pdf', upload.array("photos", 5), async (req, res) => {
 
             // Send confirmation page
             res.send(`
-                <html>
+
+                <!DOCTYPE html>
+                <html lang="ja">
                 <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
                     <title>送信完了</title>
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
                     <style>
